bece53495626b2c5d9dc8461e3d85f7e6b46ab3a
Lenny R. Briones <lennyrbriones@outlook.com>
2024-08-03 00:56:44 -0600

    feature: Principal view created, starting second view

--- IMCApp/IMCAppApp.swift
+++ IMCApp/IMCAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct IMCAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            IMCView()
         }
     }
 }
---
+++ IMCApp/IMCResult.swift
@@ -0,0 +1,20 @@
+//
+//  IMCResult.swift
+//  IMCApp
+//
+//  Created by Lenny R. Briones on 03/08/24.
+//
+
+import SwiftUI
+
+struct IMCResult: View {
+    let userWeight:Int
+    let userHeight:Int
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IMCResult(userWeight: 80, userHeight: 80)
+}

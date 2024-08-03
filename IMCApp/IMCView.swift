a38efd67d595cf7e8e45a1f45e6c87d07f231fd0
Lenny R. Briones <lennyrbriones@outlook.com>
2024-08-02 20:36:06 -0600

    build: initial commit, swift ui config

---
+++ IMCApp/IMCView.swift
@@ -0,0 +1,18 @@
+//
+//  IMCView.swift
+//  IMCApp
+//
+//  Created by Lenny R. Briones on 02/08/24.
+//
+
+import SwiftUI
+
+struct IMCView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IMCView()
+}

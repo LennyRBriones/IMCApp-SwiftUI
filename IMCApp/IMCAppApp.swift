e9d5b1930a1c4a1e973a1c529c9a879a8a4f782f
Lenny R. Briones <lennyrbriones@outlook.com>
2024-08-02 20:28:17 -0600

    Initial Commit

---
+++ IMCApp/IMCAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMCAppApp.swift
+//  IMCApp
+//
+//  Created by Lenny R. Briones on 02/08/24.
+//
+
+import SwiftUI
+
+@main
+struct IMCAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
